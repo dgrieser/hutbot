ce91c0a59cb2151905ad9fe63c3920e608db5edd
David Grieser <d.grieser@mittwald.de>
2025-09-04 14:22:52 +0200

    docs(help): add multi-config support to help message

--- bot.py
+++ bot.py
@@ -812,11 +812,19 @@ async def send_message(app: AsyncApp, channel: Channel, user: User, text: str, t
 
 async def send_help_message(app: AsyncApp, channel: Channel, user: User, thread_ts: str = "") -> None:
     help_text = (
-        "Hi! :wave: I am *Hutbot* :palm_up_hand::tophat: Here's how you can configure me via command or @mention:\n\n"
+        "Hi! :wave: I am *Hutbot* :palm_up_hand::tophat: Here's how you can configure me via /command or @mention:\n\n"
         "> * :sparkles: Now I can also handle multiple configurations per channel* :partying_face:\n>\n"
-        "> Now you can specify a config name after the command or @mention to edit a specific configuration.\n"
-        f"> All channels have a `{DEFAULT_CONFIG_NAME}` config, which also holds your previous configuration.\n"
-        f"> Commands can be uses as before. By omitting the config name, you'll edit `{DEFAULT_CONFIG_NAME}`.\n\n"
+        "> Now you can optionally specify a config name for commands to edit a specific configuration.\n"
+        "> There can be an unlimited number of configurations per channel. :rocket:\n>\n"
+        f"> All channels have a `{DEFAULT_CONFIG_NAME}` config, which also holds all previous configuration.\n>\n"
+        f"> *Commands work like before.* By omitting the config name, you'll edit `{DEFAULT_CONFIG_NAME}`.\n\n"
+        "> One use case would be to configure reply messages for *bi-directional communication*.\n>\n"
+        "> 1. Team A sends a message without a response :arrow_right: B is notified.\n"
+        "> 2. Team B sends a message without a response :arrow_right: A is notified.\n>\n"
+        "> Also, you could specify a *regex pattern to only respond to certain messages* :new:.\n"
+        "> 1. When a message contains `.*sales.*` :arrow_right: sales team is notified.\n"
+        "> 2. When a message contains `.*customer.*` :arrow_right: customer service team is notified.\n>\n"
+        "> Maybe I could also do some AI magic here in the future? :sparkles:\n>\n"
         "*Enable OpsGenie Integration:*\n"
         "```/hutbot [config] enable opsgenie\n"
         "@Hutbot [config] enable opsgenie```\n"
